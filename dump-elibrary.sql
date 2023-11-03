cfeb4149ab53623e761d6e7ac9b988b9e55ec7d3
eri.juandi <eri.juandi@neuronworks.co.id>
2023-11-03 09:09:55 +0700

    training

--- dump-elibrary.sql
+++ dump-elibrary.sql
@@ -16,6 +16,118 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `anggota`
+--
+
+DROP TABLE IF EXISTS `anggota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `anggota` (
+  `id_anggota` int NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) DEFAULT NULL,
+  `tgl_lahir` date DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `no_hp` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_anggota`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `anggota`
+--
+
+LOCK TABLES `anggota` WRITE;
+/*!40000 ALTER TABLE `anggota` DISABLE KEYS */;
+/*!40000 ALTER TABLE `anggota` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `buku`
+--
+
+DROP TABLE IF EXISTS `buku`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `buku` (
+  `id_buku` int NOT NULL AUTO_INCREMENT,
+  `judul` varchar(100) DEFAULT NULL,
+  `pengarang` varchar(100) DEFAULT NULL,
+  `stok` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_buku`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buku`
+--
+
+LOCK TABLES `buku` WRITE;
+/*!40000 ALTER TABLE `buku` DISABLE KEYS */;
+/*!40000 ALTER TABLE `buku` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `log_buku`
+--
+
+DROP TABLE IF EXISTS `log_buku`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `log_buku` (
+  `log_id` int NOT NULL AUTO_INCREMENT,
+  `id_buku` int DEFAULT NULL,
+  `tgl_log` timestamp NULL DEFAULT NULL,
+  `perubahan_stok` int DEFAULT NULL,
+  `keterangan` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`log_id`),
+  KEY `log_buku_FK` (`id_buku`),
+  CONSTRAINT `log_buku_FK` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `log_buku`
+--
+
+LOCK TABLES `log_buku` WRITE;
+/*!40000 ALTER TABLE `log_buku` DISABLE KEYS */;
+/*!40000 ALTER TABLE `log_buku` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `peminjaman`
+--
+
+DROP TABLE IF EXISTS `peminjaman`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `peminjaman` (
+  `id_pinjaman` int NOT NULL AUTO_INCREMENT,
+  `id_anggota` int DEFAULT NULL,
+  `id_buku` int DEFAULT NULL,
+  `tgl_pinjam` date DEFAULT NULL,
+  `tgl_jatuh_tempo` date DEFAULT NULL,
+  `tgl_kembali` date DEFAULT NULL,
+  `keterangan` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_pinjaman`),
+  KEY `peminjaman_FK` (`id_anggota`),
+  KEY `peminjaman_FK_1` (`id_buku`),
+  CONSTRAINT `peminjaman_FK` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`),
+  CONSTRAINT `peminjaman_FK_1` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `peminjaman`
+--
+
+LOCK TABLES `peminjaman` WRITE;
+/*!40000 ALTER TABLE `peminjaman` DISABLE KEYS */;
+/*!40000 ALTER TABLE `peminjaman` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping routines for database 'elibrary'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -28,4 +140,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-03  8:55:24
+-- Dump completed on 2023-11-03  9:08:55
